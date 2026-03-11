9a6adb4c83c43ef75e842ef4b9da6ceca711505f
shangyingzhichi <96774767+shangyingzhichi@users.noreply.github.com>
2026-03-11 17:27:56 +0800

    动态代理创建consumer 处理泛型问题，统一封装为Response

--- src/main/java/com/example/myrpc/rpc/consumer/ConsumerApp.java
+++ src/main/java/com/example/myrpc/rpc/consumer/ConsumerApp.java
@@ -1,10 +1,14 @@
 package com.example.myrpc.rpc.consumer;
 
+import com.example.myrpc.rpc.api.IAdd;
+
 public class ConsumerApp {
 
     public static void main(String[] args) throws InterruptedException {
-        Consumer consumer = new Consumer();
-        consumer.add(1, 2);
-        consumer.add(2, 3);
+        ConsumerProxyFactory proxyFactory = new ConsumerProxyFactory();
+
+        IAdd proxy = proxyFactory.createProxy(IAdd.class);
+        proxy.add(1, 2);
+        proxy.add(2, 3);
     }
 }
---
+++ src/main/java/com/example/myrpc/rpc/consumer/ConsumerProxyFactory.java
@@ -0,0 +1,136 @@
+package com.example.myrpc.rpc.consumer;
+
+import com.example.myrpc.rpc.api.IAdd;
+import com.example.myrpc.rpc.codec.MessageDecoder;
+import com.example.myrpc.rpc.codec.RequestMessageEncoder;
+import com.example.myrpc.rpc.exception.RpcException;
+import com.example.myrpc.rpc.message.Request;
+import com.example.myrpc.rpc.message.Response;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * consumer代理工厂
+ */
+@Slf4j
+public class ConsumerProxyFactory {
+    /**
+     * 异步响应（在途请求）的标准处理，<requestId, 异步响应>
+     */
+    private final Map<Integer, CompletableFuture<Response>> inFightRequestTable = new ConcurrentHashMap<>();
+    /**
+     * 连接管理器
+     */
+    private final ConnectionManager connectionManager = new ConnectionManager(createBootstrap());
+
+    private Bootstrap createBootstrap() {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline()
+                                .addLast(new MessageDecoder())  // byte[] -> Message(response)
+                                .addLast(new RequestMessageEncoder())  // message(request) -> byte[]
+                                .addLast(new SimpleChannelInboundHandler<Response>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Response response) throws Exception {
+                                        CompletableFuture<Response> completableFuture = inFightRequestTable.remove(response.getRequestId());
+                                        if (completableFuture == null) {
+                                            log.error("【{}】无法找到对应的在途请求", response.getRequestId());
+                                            return;
+                                        }
+                                        // 响应结束
+                                        completableFuture.complete(response);
+                                    }
+                                });
+                    }
+                });
+        return bootstrap;
+    }
+
+    /**
+     * 创建代理对象
+     */
+    public <I> I createProxy(Class<I> interfaceClass) {
+        Object proxyInstance = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                new Class[]{interfaceClass},
+                new ConsumerInvocationHandler()
+        );
+        return (I) proxyInstance;
+    }
+
+
+    public class ConsumerInvocationHandler implements InvocationHandler {
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            // 处理Object对象的方法
+            if (method.getDeclaringClass() == Object.class) {
+                if (method.getName().equals("equals")) {
+                    return proxy == args[0];
+                } else if (method.getName().equals("hashCode")) {
+                    return System.identityHashCode(proxy);
+                } else if (method.getName().equals("toString")) {
+                    return proxy.getClass().getName();
+                } else {
+                    throw new RpcException("不允许调用该对象的方法：" + method.getName());
+                }
+            }
+
+            // 异步容器，放provider的返回值
+            CompletableFuture<Response> resultFuture = new CompletableFuture<>();
+
+            // 连接Provider
+            try {
+                Channel channel = connectionManager.getChannel("localhost", 9999);
+                if (channel == null) {
+                    throw new RpcException("建立连接失败");
+                }
+                // 发起请求
+                Request request = new Request();
+                request.setServiceName(method.getDeclaringClass().getName());
+                request.setMethodName(method.getName());
+                request.setParamTypes(method.getParameterTypes());
+                request.setParams(args);
+
+                channel.writeAndFlush(request).addListener((future) -> {
+                    if (future.isSuccess()) {
+                        // 发送请求成功，则缓存异步响应
+                        inFightRequestTable.put(request.getRequestId(), resultFuture);
+                    }
+                });
+
+                // 等待获取Provider结果
+                // 超时5s
+                Response response = resultFuture.get(5, TimeUnit.SECONDS);
+
+                // 接收响应
+                if (Response.isSuccess(response)) {
+                    log.info("成功获取Provider结果：" + response.getResult());
+                    return response.getResult();
+                } else {
+                    throw new RpcException(response.getMsg());
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
